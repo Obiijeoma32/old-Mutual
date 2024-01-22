4102412719f95668fb132bf87309031ac530b7fb
Candy <obiijeoma32@gmail.com>
2024-01-22 18:09:41 +0100

    updated

--- src/Components/BrokerDetails.js
+++ src/Components/BrokerDetails.js
@@ -41,12 +41,12 @@ function BrokerDetails() {
       <div className="mobile:hidden lg:block ">
         <div className=" w-[100%] p-[90px] flex justify-between items-center ">
           {/* Welcome Note */}
-          <div className="  w-[45%]  ">
+          <div className="  w-[43%]  ">
             <div className=" w-[210px] ml-[-20px] mb-[32px] h-[47px] ">
               <img src={logo} alt="logo" />
             </div>
-            <h1 className="text-[44px] w-[600px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Guaranteed Insurance for individuals and Organization</h1>
-            <p className=" tracking-[1.06px] mt-[32px] text-[#808285] font-normal text-[24px] text-left opacity-[1] ">OldMutual aims to offer the best type of insurance cover that meets your needs.</p>
+            <h1 className="text-[34px] w-[600px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Guaranteed Insurance for individuals and Organization</h1>
+            <p className=" tracking-[1.06px] mt-[32px] text-[#808285] font-normal text-[20px] text-left opacity-[1] ">OldMutual aims to offer the best type of insurance cover that meets your needs.</p>
             <div className=" mt-[45px] h-[500px] ">
               <form>
                 {options.map((option) => (
@@ -59,53 +59,30 @@ function BrokerDetails() {
             </div>
           </div>
           {/* Form starts */}
-          <div className=" w-[45%]">
+          <div className=" w-[43%]">
             {/* Progress Bar */}
             <div className="flex h-2 mb-4 overflow-hidden text-xs bg-[#009677]">
-              <div style={{ width: `${progress}%` }} className="flex flex-col whitespace-nowrap  justify-center shadow-none" />
+              <div style={{ width: `${progress}%` }} className="" />
             </div>
 
-            {/* <div className="bg-[#009677] ">
-            <div className="flex mb-2 items-center justify-between">
-              {Array.from({ length: steps }).map((_, index) => (
-                <div
-                  key={index}
-                  className={classNames("w-1/4", {
-                    "bg-indigo-500": index < currentStep,
-                    "bg-gray-200": index >= currentStep,
-                  })}
-                >
-                  {index < currentStep && <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-white bg-indigo-600">{index + 1}</span>}
-                </div>
-              ))}
-            </div>
-            <div className="flex mb-2 items-center justify-between">
-              <div className="text-xs font-semibold inline-block text-indigo-600">
-                Step {currentStep} of {steps}
-              </div>
-            </div>
-          <div>
-            <div className="flex flex-col pt-1" style={{ width: `${barWidth}%` }}></div>
-          </div>
-          </div> */}
-            <div className=" w-[504px] z-[2] relative  pt-[37px] pl-[32px] h-[890px] bg-white rounded-[16px] ">
+            <div className=" w-[480px] z-[2] relative  pt-[37px] pl-[32px] h-fit pb-[50px] bg-white rounded-[16px] ">
               <div>
-                <h3 className="text-[44px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Get Started</h3>
-                <p className=" tracking-[0.88px] w-[440px] mt-[37px] text-[#282828] font-[300] text-[20px] text-left opacity-[1] ">Kindly provide the details below to create a Broker Profile</p>
-                <input className=" outline-none font-medium w-[440px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="CAC RC No" />
-                <input className=" outline-none font-medium w-[440px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="Broker ID (RB No)" />
-                <input className=" outline-none font-medium w-[440px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="Broker Name" />
-                <input className=" outline-none font-medium w-[440px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="License Issue Date" />
-                <input className=" outline-none font-medium w-[440px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="License Expiry Date" />
+                <h3 className="text-[34px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Get Started</h3>
+                <p className=" tracking-[0.88px]  mt-[37px] text-[#282828] font-[300] text-[18px] text-left opacity-[1] ">Kindly provide the details below to create a Broker Profile</p>
+                <input className=" outline-none font-medium w-[400px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="CAC RC No" />
+                <input className=" outline-none font-medium w-[400px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="Broker ID (RB No)" />
+                <input className=" outline-none font-medium w-[400px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="Broker Name" />
+                <input className=" outline-none font-medium w-[400px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="License Issue Date" />
+                <input className=" outline-none font-medium w-[400px] h-[52px] border-[0.5px] mt-[32px] text-[16px] text-[#9C9898]  pl-[24px] rounded-[4px] opacity-[1] border-[#9C9898] " type="text" placeholder="License Expiry Date" />
 
-                <div className="w-[440px] mt-[43.25px] flex justify-between items-center">
+                <div className="w-[400px] mt-[43.25px] flex justify-between items-center">
                   <Link onClick={() => window.scroll(0, 0)} to="/">
-                    <button onClick={simulateRegistration} className="w-[204px] h-[52px] text-[#50B848] text-[16px] tracking-[0.26px] font-[600]  border-[1px] border-[#50B848] opacity-[1] rounded-[8px] ">
+                    <button onClick={simulateRegistration} className="w-[180px] h-[52px] text-[#50B848] text-[16px] tracking-[0.26px] font-[600]  border-[1px] border-[#50B848] opacity-[1] rounded-[8px] ">
                       Previous
                     </button>
                   </Link>
                   <Link to="/brokeradditionalinfo">
-                    <button className="w-[204px] h-[52px] text-[#fff] text-[16px] tracking-[0.26px] font-[600]  bg-[#50B848] opacity-[1] rounded-[8px] ">Next</button>
+                    <button className="w-[180px] h-[52px] text-[#fff] text-[16px] tracking-[0.26px] font-[600]  bg-[#50B848] opacity-[1] rounded-[8px] ">Next</button>
                   </Link>
                 </div>
 
@@ -119,10 +96,10 @@ function BrokerDetails() {
             </div>
           </div>
         </div>
-        <img className=" z-[1] absolute top-[57rem] right-[30rem] " src={leaf} alt="leaf" />
+        <img className=" z-[1] absolute top-[51rem] right-[30rem] " src={leaf} alt="leaf" />
 
-        <div className=" pl-[70px] tracking-[0.38px] opacity-[1] mt-[150px] pb-[100px]">
-          <p className=" text-[24px] font-normal text-[#2E3D56CC]">
+        <div className=" pl-[70px] tracking-[0.38px] opacity-[1] mt-[100px] pb-[100px]">
+          <p className=" text-[20px] font-normal text-[#2E3D56CC]">
             Having troubles? <span className="text-[#009677] font-bold underline ">Contact Support</span>
           </p>
         </div>
--- src/Components/WelcomePage.js
+++ src/Components/WelcomePage.js
@@ -8,20 +8,20 @@ function WelcomePage() {
       <div className=" mobile:hidden lg:block  ">
         <div className=" w-full  p-[70px] flex justify-between items-center ">
           <div className="  w-[47%] ">
-            <h1 className="text-[44px] w-[600px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Welcome to OldMutual Brokers' Platform.</h1>
-            <p className=" tracking-[1.06px] mt-[32px] text-[#808285] font-normal text-[24px] text-left opacity-[1] ">
+            <h1 className="text-[34px] w-[600px] tracking-[1.94px] opacity-[1] font-bold text-[#009677] ">Welcome to OldMutual Brokers' Platform.</h1>
+            <p className=" tracking-[1.06px] mt-[20px] text-[#808285] font-normal text-[20px] text-left opacity-[1] ">
               An end to end insurance broker digital solution. Broker plaform saves time and prevents all sorts of process bottlenecks to allow brokers to request Quotes,Buy insurance, create claims, manage renewals and handle correspondence with the
               Insurer.
             </p>
-            <div className=" mt-[44px] flex justify-between w-[500px] items-center ">
+            <div className=" mt-[40px] flex justify-between w-[380px] items-center ">
               <Link to="/brokerdetails">
-                <div className=" transition ease-out duration-[0.3s] text-center tracking-[0.26px] flex justify-center items-center rounded-[8px] opacity-[1] w-[237px] h-[51px] text-[16px] font-normal text-[#fff] bg-[#50B848]  ">Get Started</div>
+                <div className=" transition ease-out duration-[0.3s] text-center tracking-[0.26px] flex justify-center items-center rounded-[8px] opacity-[1] w-[180px] h-[51px] text-[16px] font-normal text-[#fff] bg-[#50B848]  ">Get Started</div>
               </Link>
               <Link to="/login">
-                <div className=" text-center tracking-[0.26px] flex justify-center items-center rounded-[8px] opacity-[1] w-[237px] h-[51px] text-[16px] font-normal text-[#50B848] border-[1px] border-[#50B848]  ">Sign In</div>
+                <div className=" text-center tracking-[0.26px] flex justify-center items-center rounded-[8px] opacity-[1] w-[180px] h-[51px] text-[16px] font-normal text-[#50B848] border-[1px] border-[#50B848]  ">Sign In</div>
               </Link>
             </div>
-            <p className=" mt-[64px] w-[700px] text-[16px] tracking-[0.26px] opacity-[1] text-[#808285] font-normal ">
+            <p className=" mt-[64px] w-[700px] text-[14px] tracking-[0.26px] opacity-[1] text-[#808285] font-normal ">
               By clicking on sign in button, you agree to our<span className="text-[#009677]"> terms of use & privacy policy</span>{" "}
             </p>
           </div>
@@ -29,8 +29,8 @@ function WelcomePage() {
             <img className=" w-[600px]  " src={oldMutual} alt="old Mutual" />
           </div>
         </div>
-        <div className=" pl-[70px] tracking-[0.38px] opacity-[1] mt-[200px]">
-          <p className=" text-[24px] font-normal text-[#2E3D56CC]">
+        <div className=" pl-[70px] tracking-[0.38px] opacity-[1] mt-[80px]">
+          <p className=" text-[20px] font-normal text-[#2E3D56CC]">
             Having troubles? <span className="text-[#009677] font-bold ">Contact Support</span>
           </p>
         </div>
